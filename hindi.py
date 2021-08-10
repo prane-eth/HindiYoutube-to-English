c6d6c8ca740e97fe42226cf0ee19a0db8711b93c
vhpraneeth <praneeth.18bce7147@vitap.ac.in>
2021-08-10 12:29:27 +0530

    Edited files

---
+++ hindi.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+import os, sys
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+try:
+	filename = sys.argv[1]
+except:
+	filename = "sample.wav"
+r = sr.Recognizer()
+
+def transcript(filename):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)  
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk 
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+if __name__ == '__main__':
+    text = transcript(filename)
+    # translator = Translator()
+    print(text, file=open('audio.txt', 'w'))
+    # text = translator.translate(text, src='hi').text
