fafbcd1877da8e0ef4095a47a1e64c81b4e052dc
Praneeth <vhpraneeth2001@gmail.com>
2021-08-21 15:15:41 +0530

    Edited files

--- my_main.py
+++ my_main.py
@@ -96,12 +96,12 @@ def findFilename():
 
 
 def videoToEnglish(link=''):
+    print(link + '\n', file=open('last_translated.txt', 'w'))
     download_video(link)
     audioname = findFilename()
     whole_text = transcript(audioname)
     whole_text = hindiToEnglish(whole_text)
-    print(link + '\n' + whole_text,
-            file=open('last_translated.txt', 'w'))
+    print(whole_text, file=open('last_translated.txt', 'w'))
     return whole_text
 
 
